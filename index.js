28c4136fb21679ed7450a71c06e20f56342770cf
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-13 11:33:55 +0100

    Added validation for zip and country tags
    Changed countries array to object with country name as key and value of array with a regex and error message
    
    switches generation of option elements loop from forEach for a regular for loop
    
    added functions checkZip and selectedCountry
    
    called checkZip with value returned from selectedCountry on zip input and countrySelector click  event listeners

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ const passwordConfirmation = document.getElementById("password-confirmation");
 const submitBtn = document.getElementById("btn-submit");
 
 const elements = [email, country, zip, password, passwordConfirmation];
-const countries = [
-  "United States",
-  "Switzerland",
-  "France",
-  "Germany",
-  "The Netherlands",
-];
+const countries = {
+  "United States": [
+    new RegExp("^\\d{5}(?:[-\\s]\\d{4})?$"),
+    "U.S. ZIP codes must be 5 digits or 5+4 digits: e.g. 12345 or 12345-6789",
+  ],
+  Switzerland: [
+    new RegExp("^(CH-)?\\d{4}$"),
+    "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+  ],
+  France: [
+    new RegExp("^(F-)?\\d{5}$"),
+    "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+  ],
+  Germany: [
+    new RegExp("^(D-)?\\d{5}$"),
+    "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
+  ],
+  "The Netherlands": [
+    new RegExp("^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$"),
+    "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+  ],
+};
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -24,8 +39,34 @@ submitBtn.addEventListener("click", (e) => {
   });
 });
 
-countries.forEach((country) => {
+for (const country in countries) {
   let temp = document.createElement("option");
   temp.textContent = country;
   countrySelector.appendChild(temp);
+}
+
+const checkZip = (country) => {
+  const constraint = countries[country][0];
+
+  if (constraint.test(zip.value)) {
+    zip.setCustomValidity("");
+  } else {
+    zip.setCustomValidity(countries[country][1]);
+  }
+};
+
+const selectedCountry = () => {
+  for (const country of document.querySelectorAll("option")) {
+    if (country.selected == true) {
+      return country.textContent;
+    }
+  }
+};
+
+zip.addEventListener("input", () => {
+  checkZip(selectedCountry());
+});
+
+countrySelector.addEventListener("click", () => {
+  checkZip(selectedCountry());
 });

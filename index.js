5a6baf83a9266ea79b33fab9b23d2e0507eaa0de
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-13 10:35:17 +0100

    Added country options inside the select tag using the DOM
    changed name of country selector to countrySelector
    
    added array with countries

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const email = document.getElementById("email");
-const country = document.getElementById("country");
+const countrySelector = document.getElementById("country");
 const zip = document.getElementById("zip");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
 const submitBtn = document.getElementById("btn-submit");
 
 const elements = [email, country, zip, password, passwordConfirmation];
+const countries = [
+  "United States",
+  "Switzerland",
+  "France",
+  "Germany",
+  "The Netherlands",
+];
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -15,4 +22,10 @@ submitBtn.addEventListener("click", (e) => {
       console.log("Required field cannot be empty");
     }
   });
+});
+
+countries.forEach((country) => {
+  let temp = document.createElement("option");
+  temp.textContent = country;
+  countrySelector.appendChild(temp);
 });
